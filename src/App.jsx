76c2d71cdff85f327f3a4340f75ff4cd8a1ef959
Jeff Beltran <jeffbeltran@hey.com>
2021-02-15 05:45:33 -0800

    feat: add React Query to simplify hook API
    This will just turn the hooks into a nice React Query wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import { useNextLaunch } from "../lib/hooks";
 // import { useNextLaunch } from "@jeffbeltran/vega";
 
 function App() {
-  const { isLoading, nextLaunch, getNextLaunch } = useNextLaunch();
+  const { data: nextLaunch, isLoading, refetch } = useNextLaunch();
 
   return (
     <div>
       <h1>Next Launch</h1>
       <h2>{isLoading ? `loading` : `loaded`}</h2>
-      <button onClick={getNextLaunch}>Get Info</button>
+      <button onClick={refetch}>Get Info</button>
       <ReactJson src={nextLaunch} name={false} />
     </div>
   );
