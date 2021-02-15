066d4cb0530c26f67e58fc40e53f0465908277b1
Jeff Beltran <jeffbeltran@hey.com>
2021-02-14 17:35:01 -0800

    feat: add hook for getting next launch

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,19 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from "react";
+import ReactJson from "react-json-view";
+import { useNextLaunch } from "../lib/hooks";
+// import { useNextLaunch } from "@jeffbeltran/vega";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { isLoading, nextLaunch, getNextLaunch } = useNextLaunch();
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div>
+      <h1>Next Launch</h1>
+      <h2>{isLoading ? `loading` : `loaded`}</h2>
+      <button onClick={getNextLaunch}>Get Info</button>
+      <ReactJson src={nextLaunch} name={false} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
-})
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "lib/main.js"),
+      name: "Vega",
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["react"],
+    },
+  },
+  plugins: [reactRefresh()],
+});
